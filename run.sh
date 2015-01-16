193bfeddd6ef326d61c95e5796e7d3c995bbba2a
Peter Baumgartner <pete@lincolnloop.com>
2015-01-16 14:27:55 -0700

    Default to a hostname that might be running ES

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-KIBANA_ES_URL=${KIBANA_ES_URL:-http://localhost:9200}
+KIBANA_ES_URL=${KIBANA_ES_URL:-http://elasticsearch:9200}
 KIBANA_INDEX=${KIBANA_INDEX:-kibana-int}
 
 sed -i "s;^elasticsearch:.*;elasticsearch: ${KIBANA_ES_URL};" "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
