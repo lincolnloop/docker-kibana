d6bd87f532c8dfd25a577ebaae6ecaecbac2bdbd
Peter Baumgartner <pete@lincolnloop.com>
2015-03-20 09:25:54 -0600

    4.0.1

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
 FROM java:8
 
-ENV KIBANA_VERSION 4.0.0-beta3
+ENV KIBANA_VERSION 4.0.1
+ENV MD5_SUM ae5019e3c36a03fc57a069c235ca97e4
 
-RUN curl -s https://download.elasticsearch.org/kibana/kibana/kibana-${KIBANA_VERSION}.tar.gz | tar zx -C /opt
+WORKDIR /opt
+RUN curl -O https://download.elasticsearch.org/kibana/kibana/kibana-${KIBANA_VERSION}-linux-x64.tar.gz
+RUN echo "${MD5_SUM}  kibana-${KIBANA_VERSION}-linux-x64.tar.gz" > kibana.md5 && md5sum --check kibana.md5
+RUN tar xzf kibana-${KIBANA_VERSION}-linux-x64.tar.gz && rm kibana-${KIBANA_VERSION}-linux-x64.tar.gz
 
 ADD ./run.sh /run.sh
 
--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@ set -e
 
 KIBANA_ES_URL=${KIBANA_ES_URL:-http://elasticsearch:9200}
 KIBANA_INDEX=${KIBANA_INDEX:-kibana-int}
+KIBANA_DIR=/opt/kibana-${KIBANA_VERSION}-linux-x64
 
-sed -i "s;^elasticsearch:.*;elasticsearch: ${KIBANA_ES_URL};" "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
-sed -i "s;^kibana_index:.*;kibana_index: ${KIBANA_INDEX};" "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
+sed -i "s;^elasticsearch:.*;elasticsearch: ${KIBANA_ES_URL};" "${KIBANA_DIR}/config/kibana.yml"
+sed -i "s;^kibana_index:.*;kibana_index: ${KIBANA_INDEX};" "${KIBANA_DIR}/config/kibana.yml"
 
-exec /opt/kibana-${KIBANA_VERSION}/bin/kibana
+exec ${KIBANA_DIR}/bin/kibana
