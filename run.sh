40bfa82c447c95e5c5c85b9f09ef0cba4ea162fe
Peter Baumgartner <pete@lincolnloop.com>
2015-03-21 19:31:21 -0600

    New setting

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ KIBANA_ES_URL=${KIBANA_ES_URL:-http://elasticsearch:9200}
 KIBANA_INDEX=${KIBANA_INDEX:-kibana-int}
 KIBANA_DIR=/opt/kibana-${KIBANA_VERSION}-linux-x64
 
-sed -i "s;^elasticsearch:.*;elasticsearch: ${KIBANA_ES_URL};" "${KIBANA_DIR}/config/kibana.yml"
+sed -i "s;^elasticsearch_url:.*;elasticsearch_url: ${KIBANA_ES_URL};" "${KIBANA_DIR}/config/kibana.yml"
 sed -i "s;^kibana_index:.*;kibana_index: ${KIBANA_INDEX};" "${KIBANA_DIR}/config/kibana.yml"
 
 exec ${KIBANA_DIR}/bin/kibana
