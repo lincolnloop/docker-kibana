6c097b6bfb93fc2bc1e81d031b1bcfc40b24bf22
Peter Baumgartner <pete@lincolnloop.com>
2016-10-04 13:18:31 +0100

    Fixed typo

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ set -e
 KIBANA_ES_URL=${KIBANA_ES_URL:-http://elasticsearch:9200}
 KIBANA_DIR=/opt/kibana
 
-echo "elasticsearch_url: ${KIBANA_ES_URL};" >> "${KIBANA_DIR}/config/kibana.yml"
+echo "elasticsearch_url: ${KIBANA_ES_URL}" >> "${KIBANA_DIR}/config/kibana.yml"
 
 exec ${KIBANA_DIR}/bin/kibana
